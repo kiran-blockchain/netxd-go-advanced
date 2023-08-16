f536e446166d2918c4d0678f182e7e295ee132f0
BRUGUSOFT <kiran.paturi@brugu.io>
2023-08-16 18:30:14 +0530

    added code to fetch the records

--- mongodb-dal/main.go
+++ mongodb-dal/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"mongodb-dal/models"
+	//"mongodb-dal/models"
 	"mongodb-dal/services"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	//"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,9 +19,13 @@ func main() {
 	fmt.Println("MongoDB successfully connected...")
 	// product:= models.Product{ID: primitive.NewObjectID(),Name: "Samsung",Price: 1000000,Description: "Budget Phone"}
 	// services.InsertProduct(product)
-	products := []interface{}{models.Product{ID: primitive.NewObjectID(), Name: "OnePlus", Price: 1000000, Description: "Budget Phone"},
-
-		models.Product{ID: primitive.NewObjectID(), Name: "Vivo", Price: 100000, Description: "China Phone"},}
-		services.InsertProductList(products)
+	// products := []interface{}{
+	// 	models.Product{ID: primitive.NewObjectID(), Name: "OnePlus", Price: 1000000, Description: "Budget Phone"},
+	// 	models.Product{ID: primitive.NewObjectID(), Name: "Vivo", Price: 100000, Description: "China Phone"}}
+	// services.InsertProductList(products)
+	products,_ := services.FindProducts()
+    for _,product := range products{
+		fmt.Println(product)
+	 }
 
 }
